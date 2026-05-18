57ce1a7e4efdad78672d9b0819a147fbe30124c1
keokistevenson <keokistevenson@gmail.com>
2026-05-18 02:47:41 -0400

    Commiting unfinished draft code in TodoInput. Will revisit later.

--- src/components/TodoInput.tsx
+++ src/components/TodoInput.tsx
@@ -1,6 +1,14 @@
+import { useState } from "react";
+
 function TodoInput() {
+    const [state, setState] = useState<string>("");
+
+    function addTodo = () => {
+        setState(todoInput.value);
+    }
+
     return <section>
-        <input type="text" /> <button>Add Todo</button>
+        <input className="todoInput" type="text" /> <button onclick = addTodo>Add Todo</button>
     </section>
 }
 
