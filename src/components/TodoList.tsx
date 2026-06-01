bf199e5899342cabdb40cf85e271cf99ec7330de
keokistevenson <keokistevenson@gmail.com>
2026-06-01 15:41:52 -0400

    Updated TodoList using useContext useTodos.

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,44 +1,14 @@
 import TodoItem from "./TodoItem";
-import type { Todo } from "../types/Todo";
+import { useTodos } from "../context/TodoContext";
 
-interface TodoListProps {
-    todos: Todo[];
-    setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
-}
 
-function TodoList({ todos, setTodos }: TodoListProps) {
+function TodoList() {
+const { todos, clearCompleted } = useTodos();
+
     // Derived values don't need State
     // They are recalculated every time TodoList renders.
     const itemsLeft = todos.filter((todo) => !todo.completed);
     const itemsCompleted = todos.filter((todo) => todo.completed);
-
-    function deleteTodo(id: number) {
-        setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
-    }
-
-    function toggleTodo(id: number) {
-        setTodos((prevTodos) =>
-            prevTodos.map((todo) =>
-                todo.id === id
-                    ? { ...todo, completed: !todo.completed }
-                    : todo
-            )
-        );
-    }
-
-    function editTodo(id: number, newText: string) {
-        setTodos((prevTodos) =>
-            prevTodos.map((todo) =>
-                todo.id === id ? { ...todo, text: newText } : todo
-            )
-        );
-    }
-
-    function clearCompleted() {
-
-        // Code below is better than setTodos(itemsLeft); because the code below gets the latest state from React.
-        setTodos((prevTodos) => prevTodos.filter((todo) => !todo.completed));
-    }
 
     return (
         <section>
@@ -47,9 +17,9 @@ function TodoList({ todos, setTodos }: TodoListProps) {
                     <TodoItem
                         key={todo.id}
                         todo={todo}
-                        onDeleteTodo={deleteTodo}
-                        onToggleTodo={toggleTodo}
-                        onEditTodo={editTodo}
+                        // onDeleteTodo={deleteTodo}
+                        // onToggleTodo={toggleTodo}
+                        // onEditTodo={editTodo}
                     />
                 ))}
             </ul>
